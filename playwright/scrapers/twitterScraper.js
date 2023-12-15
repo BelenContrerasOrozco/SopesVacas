5661bab18f728665c3f6c60e450403691118ea87
BelenContrerasOrozco <3007300040101@ingenieria.usac.edu.gt>
2023-12-15 17:16:34 -0600

    Practica 1

---
+++ playwright/scrapers/twitterScraper.js
@@ -0,0 +1,26 @@
+// Archivo: scrapers/twitterScraper.js
+
+const { firefox } = require('playwright');
+
+const scrapeLinkedIn = async (url) => {
+  const browser = await firefox();
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  try {
+    await page.goto(url, { waitUntil: 'domcontentloaded' });
+
+    // Lógica específica de LinkedIn
+    const linkedInInfo = await page.$eval('.linkedin-info', el =>
+      el.textContent.trim()
+    );
+
+    return linkedInInfo;
+  } catch (error) {
+    console.error('Error al momento de hacer scraping:', error);
+  } finally {
+    await browser.close();
+  }
+};
+
+module.exports = scrapeLinkedIn;
